f3b3fed709c5e8130de089711d76730a2a4bef36
Hadeel-wd <hadkh89@gmail.com>
2020-01-14 13:49:23 -0500

    Updates

---
+++ precache-manifest.7d660862221e6c56f5c50cc2401b8bf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae8f875835586bf8bbd1c8458373c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "de0036646053c5d3e62d",
+    "url": "/static/css/main.33575798.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "de0036646053c5d3e62d",
+    "url": "/static/js/main.11715059.chunk.js"
+  },
+  {
+    "revision": "05efcc9d811b4a2a3c6e",
+    "url": "/static/js/runtime-main.8f8b62bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
